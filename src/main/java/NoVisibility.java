3ac4a86777229ff549761a0b3b8bab92f2412e02
Vishal Kriplani <48404770+Vk76@users.noreply.github.com>
2022-08-15 13:59:09 +0530

    non visibility tests

---
+++ src/main/java/NoVisibility.java
@@ -0,0 +1,20 @@
+public class NoVisibility {
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread{
+        public void run(){
+            while (!ready)Thread.yield();
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 500; i++) {
+            new ReaderThread().start();
+        }
+        ready = true;
+        Thread.sleep(1);
+        number = 42;
+    }
+}
